a340216209e4e1e6d712e8e4d88a2688a43eb04d
Leon Kuijf <leon.kuijf@gmail.com>
2022-11-17 18:22:37 +0100

    meta name="referrer" content="strict-origin-when-crossorigin"

--- public/postForm.php
+++ public/postForm.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <meta name="referrer" content="strict-origin-when-crossorigin">
 </head>
 <body>
     <form action="postToMe.php" method="post">
