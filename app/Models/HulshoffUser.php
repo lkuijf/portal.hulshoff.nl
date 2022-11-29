8342e404d1dd6bb7f40b01d7d72e38075d4fd9ee
Leon Kuijf <leon.kuijf@gmail.com>
2022-11-29 10:57:59 +0100

    made the HulshoffUser Notifiable

--- app/Models/HulshoffUser.php
+++ app/Models/HulshoffUser.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class HulshoffUser extends Authenticatable
 {
-    use HasFactory, TwoFactorAuthenticatable;
+    use HasFactory, TwoFactorAuthenticatable, Notifiable;
 
     public function canDisplay() {
         if(!$this->is_admin && !$this->klantCode) {
