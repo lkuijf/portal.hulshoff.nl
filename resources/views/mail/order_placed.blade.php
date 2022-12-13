5ca9866d4f444c2f8fc856b0d67e29cb223b3dc5
Leon Kuijf <leon.kuijf@gmail.com>
2022-12-13 11:40:43 +0100

    data to email view

--- resources/views/mail/order_placed.blade.php
+++ resources/views/mail/order_placed.blade.php
@@ -1,5 +1,10 @@
 @extends('templates.email')
 @section('content')
-    <h1>Order/reservation placed</h1>
-    <p>Your order or reservation has been placed.</p>
+    @if ($isReservation)
+        <h1>Reservation placed</h1>
+        <p>Your reservation has been placed.</p>
+    @else
+        <h1>Order placed</h1>
+        <p>Your order has been placed.</p>
+    @endif
 @endsection
