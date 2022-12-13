a81d506607ba7819094edfa4188be5c24b6bb866
Leon Kuijf <leon.kuijf@gmail.com>
2022-12-13 10:27:39 +0100

    email template added

--- resources/views/mail/order_promoted.blade.php
+++ resources/views/mail/order_promoted.blade.php
@@ -1,1 +1,5 @@
-<h1>Your order has been promoted</h1>
+@extends('templates.email')
+@section('content')
+    <h1>Order promoted</h1>
+    <p>Your reservation has been promoted to an order</p>
+@endsection
