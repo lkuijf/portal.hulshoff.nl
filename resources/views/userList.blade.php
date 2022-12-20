5c5d38ced4e6a8d6d47349078a6ac24286ffab17
Leon Kuijf <leon.kuijf@gmail.com>
2022-12-20 15:44:27 +0100

    thead tbody user table

--- resources/views/userList.blade.php
+++ resources/views/userList.blade.php
@@ -7,39 +7,43 @@
 {{-- <p><a href="{{ route('new_user') }}">[new user]</a></p> --}}
 <p><a href="@if($data['type'] == 'users'){{ route('new_user') }}@elseif($data['type'] == 'admins'){{ route('new_admin') }}@endif">[new {{ substr($data['type'], 0, -1) }}]</a></p>
 <table>
-    <tr>
-        <th>Name</th>
-        <th>E-mail adres</th>
-        <th>Klant</th>
-        <th>Extra E-mail adressen</th>
-        <th>Privileges</th>
-        <th>Can reserve?</th>
-        <th>&nbsp;</th>
-    </tr>
-    @foreach ($data['users'] as $user)
-    <tr>
-        <td>{{ $user->name }}</td>
-        <td>{{ $user->email }}</td>
-        <td>{{ $user->klantCode }}</td>
-        <td>@if($user->extra_email !== null){{ implode(', ', array_column(json_decode($user->extra_email,true),'email')) }}@endif</td>
-        <td>@if($user->privileges !== null){{ implode(', ', json_decode($user->privileges,true)) }}@endif</td>
-        {{-- @if($user->privileges !== null)@dd($user->privileges)@endif --}}
-        {{-- <td>@if($user->privileges !== null){{ implode(', ', json_decode('[aasasd,66]',true)) }}@endif</td> --}}
-        <td>{{ $user->can_reserve?'Ja':'Nee' }}</td>
-        <td>
-            <a href="{{ route('users') }}/{{ $user->id }}">[edit]</a>
-            {{-- <a href="">[remove]</a> --}}
-            <form action="/user" method="post">
-                @method('delete')
-                @csrf
-                <input type="hidden" name="id" value="{{ $user->id }}">
-                @if ($user->id != 1)
-                    <button type="submit" onclick="return confirm('You are about to delete user {{ $user->name }} ({{ $user->email }})\n\nAre you sure?')">Delete</button>
-                @endif
-            </form>
-        </td>
-    </tr>
-    @endforeach
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>E-mail adres</th>
+            <th>Klant</th>
+            <th>Extra E-mail adressen</th>
+            <th>Privileges</th>
+            <th>Can reserve?</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($data['users'] as $user)
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->klantCode }}</td>
+            <td>@if($user->extra_email !== null){{ implode(', ', array_column(json_decode($user->extra_email,true),'email')) }}@endif</td>
+            <td>@if($user->privileges !== null){{ implode(', ', json_decode($user->privileges,true)) }}@endif</td>
+            {{-- @if($user->privileges !== null)@dd($user->privileges)@endif --}}
+            {{-- <td>@if($user->privileges !== null){{ implode(', ', json_decode('[aasasd,66]',true)) }}@endif</td> --}}
+            <td>{{ $user->can_reserve?'Ja':'Nee' }}</td>
+            <td>
+                <a href="{{ route('users') }}/{{ $user->id }}">[edit]</a>
+                {{-- <a href="">[remove]</a> --}}
+                <form action="/user" method="post">
+                    @method('delete')
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $user->id }}">
+                    @if ($user->id != 1)
+                        <button type="submit" onclick="return confirm('You are about to delete user {{ $user->name }} ({{ $user->email }})\n\nAre you sure?')">Delete</button>
+                    @endif
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
 </table>
 {{-- @else
 GEEN TOEGANG
