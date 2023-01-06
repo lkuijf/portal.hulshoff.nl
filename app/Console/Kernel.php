8aff37727e1993b08e1c3bbadd13616a2c82bb5a
Leon Kuijf <leon.kuijf@gmail.com>
2023-01-06 09:46:34 +0100

    everyMinute() test if error is outputted

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new ArchiveXml)->dailyAt(1);
+        // $schedule->job(new ArchiveXml)->dailyAt(1);
+        $schedule->job(new ArchiveXml)->everyMinute(1);
         $schedule->job(new ParseArtikelXml)->hourly(); // dailyAt('14:23')
         $schedule->job(new ParseKlantXml)->hourly();
         $schedule->job(new ParseOrderXml)->hourly();
