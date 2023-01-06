3ad8b45f8ab17d687eac9f6836074ea482ac60d4
Leon Kuijf <leon.kuijf@gmail.com>
2023-01-06 10:20:10 +0100

    Stringable $error

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,27 +22,18 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->job(new ArchiveXml)->dailyAt(1);
-        // $schedule->job(new ArchiveXml)->everyMinute()->onFailure(function () {
-        //     Mail::raw('some error code...', function ($message) {
-        //         $message
-        //           ->to('leon@wtmedia-events.nl')
-        //           ->subject('ArchiveXml job failed!');
-        //       });
-        // });
-        try {
-            $schedule->job(new ArchiveXml)->everyMinute()->emailOutputOnFailure('leon@wtmedia-events.nl');
-            $schedule->job(new ParseArtikelXml)->hourly(); // dailyAt('14:23')
-            $schedule->job(new ParseKlantXml)->hourly();
-            $schedule->job(new ParseOrderXml)->hourly();
-            $schedule->job(new ParseVoorraadXml)->hourly();
-        } catch (\Exception $e) {
-            // $data->error = $e->getMessage();
-            Mail::raw($e->getMessage(), function ($message) {
+        $schedule->job(new ArchiveXml)->everyMinute()->onFailure(function (Stringable $error) {
+            Mail::raw($error, function ($message) {
                 $message
                   ->to('leon@wtmedia-events.nl')
-                  ->subject('Hulshoff portal job failed!');
+                  ->subject('ArchiveXml job failed!');
               });
-        }
+        });
+        // $schedule->job(new ArchiveXml)->everyMinute()->emailOutputOnFailure('leon@wtmedia-events.nl');
+        $schedule->job(new ParseArtikelXml)->hourly(); // dailyAt('14:23')
+        $schedule->job(new ParseKlantXml)->hourly();
+        $schedule->job(new ParseOrderXml)->hourly();
+        $schedule->job(new ParseVoorraadXml)->hourly();
     }
 
     /**
