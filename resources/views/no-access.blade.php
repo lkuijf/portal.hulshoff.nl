0002a45f12ca520811b96a8e586dde7756dc33a1
Leon Kuijf <leon.kuijf@gmail.com>
2023-01-25 08:18:01 +0100

    noDataContent wrapper

--- resources/views/no-access.blade.php
+++ resources/views/no-access.blade.php
@@ -1,5 +1,7 @@
 @extends('templates.portal')
 @section('content')
-    <h1>Toegang geweigerd</h1>
-    <p>U bent niet gemachtigd om deze pagina te bekijken.</p>
+    <div class="noAccessContent">
+        <h1>Toegang geweigerd</h1>
+        <p>U bent niet gemachtigd om deze pagina te bekijken.</p>
+    </div>
 @endsection
