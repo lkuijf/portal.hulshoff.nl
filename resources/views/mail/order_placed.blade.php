4250a97b2ae47b277c042a49e180726989d7e252
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-04 09:56:07 +0200

    added date

--- resources/views/mail/order_placed.blade.php
+++ resources/views/mail/order_placed.blade.php
@@ -3,12 +3,13 @@
     @if ($order->is_reservation)
         <h1>Reservation placed</h1>
         <p>Your reservation has been placed.</p>
-        <p>{{ $order->id }}</p>
-        <p>{{ $order->orderCodeKlant }}</p>
+        <p>Reservation number: {{ $order->id }}</p>
+        <p>Reservation code klant{{ $order->orderCodeKlant }}</p>
     @else
         <h1>Order placed</h1>
         <p>Your order has been placed.</p>
-        <p>{{ $order->id }}</p>
-        <p>{{ $order->orderCodeKlant }}</p>
+        <p>Order number: {{ $order->id }}</p>
+        <p>Order code klant{{ $order->orderCodeKlant }}</p>
     @endif
+    <p>Delivery date: {{ date('d-m-Y', strtotime($order->afleverDatum)) }}</p>
 @endsection
