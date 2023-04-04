a0f51011f636c2cc56325536463aceb397639097
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-04 10:27:53 +0200

    laat maar even zo

--- resources/views/templates/email.blade.php
+++ resources/views/templates/email.blade.php
@@ -1,18 +1,19 @@
 <html><body>
     <!--[if mso]>
-    <table cellpadding="0" cellspacing="0" border="0" style="padding:0px;margin:0px;width:90%;">
+    <table cellpadding="0" cellspacing="0" border="0" style="padding:0px;margin:0px;width:100%;">
         <tr>
             <td style="padding:0px;margin:0px;">&nbsp;</td>
-            <td style="padding:0px;margin:0px;" width="700">
+            <td style="padding:0px;margin:0px;" width="500">
     <![endif]-->
                 <div style="
-                    max-width: 700px;
+                    max-width: 500px;
                     padding: 20px;
                     font-family: verdana, arial;
                     font-size: 14px;
                     margin-left: auto;
                     margin-right: auto;
-                    background-color: #EFEFEF;
+                    background-color: #FFF;
+                    border: 2px solid #CCC;
                 ">
                 <p style="text-align:center;"><img src="https://portal.hulshoff.nl/statics/hulshoff-logo.png" alt="logo" /></p>
 				@yield('content')
