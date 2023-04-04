935222e1fd7490dd183211e7df00dbe3293c8c36
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-04 10:07:16 +0200

    OrderArticle model

--- app/Mail/OrderPlaced.php
+++ app/Mail/OrderPlaced.php
@@ -9,6 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Order;
+use App\Models\OrderArticle;
 use App\Models\Product;
 
 class OrderPlaced extends Mailable
