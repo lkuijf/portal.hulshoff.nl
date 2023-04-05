ccd338cc6bcf49e129b2469201d765f545223234
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-05 13:54:49 +0200

    reservation promoted better structure

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -124,7 +124,15 @@ class OrderController extends Controller
         }
         $order->save();
 
-        if($request->type == 'confirmReservation') Mail::to(auth()->user()->email)->send(new OrderPromoted($order));
+        if($request->type == 'confirmReservation') {
+            Mail::to(auth()->user()->email)->send(new OrderPromoted($order));
+            $extraEmails = json_decode(auth()->user()->extra_email);
+            if($extraEmails && count($extraEmails)) {
+                foreach($extraEmails as $e_email) {
+                    Mail::to($e_email)->send(new OrderPromoted($order));
+                }
+            }
+        }
 
         if($request->type == 'confirmReservation') $request->session()->flash('message', '<p>' . __('Your order has been placed') . '!</p>');
         if($request->type == 'updateDeliveryDate') $request->session()->flash('message', '<p>' . __('Delivery date has been changed') . '</p>');
--- resources/views/mail/order_promoted.blade.php
+++ resources/views/mail/order_promoted.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.email')
 @section('content')
     <h1>Reservation promoted</h1>
-    <p>Your reservation has been promoted to an order</p>
+    <p>Your reservation has been promoted to an order. The products you selected will be delivered on the date you specified. Below you will find details of your order.</p>
     <p>Order number:<br /><strong>{{ $order->id }}</strong></p>
     <p>Order code klant:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
     <p>Delivery date:<br /><strong>{{ date('d-m-Y', strtotime($order->afleverDatum)) }}</strong></p>
