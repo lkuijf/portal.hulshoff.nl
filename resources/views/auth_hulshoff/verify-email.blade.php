c44f2f844e3e3a0b9316bf41026e67a2a8e26b76
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-06 15:09:51 +0200

    verification lang

--- resources/views/auth_hulshoff/verify-email.blade.php
+++ resources/views/auth_hulshoff/verify-email.blade.php
@@ -1,27 +1,13 @@
 <div class="verifyEmailContent">
-    <h1>Verify your e-mail address</h1>
-    <p>Send an e-mail with a verification link, click on this button:</p>
+    <h1>{{ __('Verify your e-mail address') }}</h1>
+    <p>{{ __('Send an e-mail with a verification link') }}, {{ __('click on this button') }}:</p>
     <form action="/email/verification-notification" method="POST">
         @csrf
-        {{-- <input type="text" name="email"> --}}
-        <button type="submit">Send e-mail</button>
+        <button type="submit">{{ __('Send e-mail') }}</button>
     </form>
-    <p>Please verify your e-mail address by clicking the link in the e-mail message.</p>
-    <p><a href="{{ route('login') }}">< Terug</a></p>
+    <p>{{ __('Please verify your e-mail address by clicking the link in the e-mail message') }}.</p>
+    <p><a href="{{ route('login') }}">< {{ __('Back') }}</a></p>
     @if (session('status') == 'verification-link-sent')
-        {{-- <p>A new email verification link has been emailed to you!</p> --}}
-        <script>showMessage('success',"<p>A new email verification link has been emailed to you!</p>")</script>
+        <script>showMessage('success',"<p>{{ __('An email address verification link has been emailed to you') }}!</p>")</script>
     @endif
-    {{-- @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-    @if (session('status'))
-        <ul>
-            <li>{{ session('status') }}</li>
-        </ul>
-    @endif --}}
 </div>
