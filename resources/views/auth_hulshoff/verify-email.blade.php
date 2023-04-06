112c5032b978b162cd6630911599902239132e46
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-06 15:11:22 +0200

    back weg

--- resources/views/auth_hulshoff/verify-email.blade.php
+++ resources/views/auth_hulshoff/verify-email.blade.php
@@ -6,7 +6,7 @@
         <button type="submit">{{ __('Send e-mail') }}</button>
     </form>
     <p>{{ __('Please verify your e-mail address by clicking the link in the e-mail message') }}.</p>
-    <p><a href="{{ route('login') }}">< {{ __('Back') }}</a></p>
+    {{-- <p><a href="{{ route('login') }}">< {{ __('Back') }}</a></p> --}}
     @if (session('status') == 'verification-link-sent')
         <script>showMessage('success',"<p>{{ __('An email address verification link has been emailed to you') }}!</p>")</script>
     @endif
