690d479703623178c0022235a433947bb4b842e5
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-12 17:01:19 +0200

    test check

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,9 +30,9 @@ class AuthServiceProvider extends ServiceProvider
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
-                ->subject('Verify Email Address')
+                ->subject('Verify Email Address2')
                 ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address', $url);
+                ->action('Verify Email Address2', $url);
         });
     }
 }
