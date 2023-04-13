8b7e63fc625e2820f980ae0b6ed3acd60963939e
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 11:14:01 +0200

    button black and logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,8 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-{{ $slot }}
+{{-- <p>{{ $slot }}</p> --}}
+<img src="{{ url('statics/hulshoff-logo.png') }}" alt="logo" />
 @endif
 </a>
 </td>
