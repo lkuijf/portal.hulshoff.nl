3ddb50259c308ea331d216bf26a07686db93ca94
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 11:40:26 +0200

    2 rules did not work. html?

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,8 +34,7 @@ class AuthServiceProvider extends ServiceProvider
                 ->subject(__('Verify Email Address'))
                 ->line(__('Click the button below to verify your email address') . '.')
                 ->action(__('Verify Email Address'), $url)
-                ->salutation(__('Regards') . ',')
-                ->salutation('Hulshoff')
+                ->salutation(__('Regards') . ',<br />Hulshoff')
                 ;
         });
     }
