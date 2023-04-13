0d70685e90af38b0f661bcc8aa17c139a02e9665
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 14:10:04 +0200

    new address for xml

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,6 +18,10 @@ class Order extends Model
         return $this->hasOne(HulshoffUser::class, 'id', 'hulshoff_user_id');
     }
 
+    public function customer() {
+        return $this->hasOne(Customer::class, 'klantCode', 'klantCode');
+    }
+
     public function generateXml() {
         $aAdressen = [];
         $aDetails = [];
@@ -26,26 +30,27 @@ class Order extends Model
         $orderXml->addChild('bericht-id', $this->id);
         $orders = $orderXml->addChild('orders');
         $order = $orders->addChild('order');
-        $order->addChild('ord-klant-code', ($this->hulshoffUser?$this->hulshoffUser->klantCode:'-deleted user-'));
+        // $order->addChild('ord-klant-code', ($this->hulshoffUser?$this->hulshoffUser->klantCode:'-deleted user-'));
+        $order->addChild('ord-klant-code', $this->klantCode);
         $order->addChild('ord-order-code-klant', $this->orderCodeKlant);
         $order->addChild('ord-order-code-aflever', $this->orderCodeAflever);
         $order->addChild('ord-eta-aflever-datum', $this->afleverDatum);
         $order->addChild('ord-eta-aflever-tijd', $this->afleverTijd);
         $adressen = $order->addChild('adressen');
 
-        // if($this->hulshoffUser->customer) { // when the user is connected to a customer
-        //     $aAdressen[0] = $adressen->addChild('adres');
-        //     $aAdressen[0]->addChild('afa-afleveradres-code', 'ADRES1'); // ??
-        //     $aAdressen[0]->addChild('afa-naam', $this->hulshoffUser->customer->straat);
-        //     $aAdressen[0]->addChild('afa-straat', $this->hulshoffUser->customer->straat);
-        //     $aAdressen[0]->addChild('afa-huisnummer', $this->hulshoffUser->customer->huisnummer);
-        //     $aAdressen[0]->addChild('afa-postcode', $this->hulshoffUser->customer->postcode);
-        //     $aAdressen[0]->addChild('afa-plaats', $this->hulshoffUser->customer->plaats);
-        //     $aAdressen[0]->addChild('afa-land-code', $this->hulshoffUser->customer->landCode);
-        //     $aAdressen[0]->addChild('afa-contactpersoon', $this->hulshoffUser->customer->contactpersoon);
-        //     $aAdressen[0]->addChild('afa-telefoon', $this->hulshoffUser->customer->telefoon);
-        //     $aAdressen[0]->addChild('afa-e-mailadres', $this->hulshoffUser->customer->eMailadres);
-        // }
+        if($this->customer) { // when the order is connected to a customer
+            $aAdressen[0] = $adressen->addChild('adres');
+            $aAdressen[0]->addChild('afa-afleveradres-code', 'ALGEMEEN'); // ??
+            $aAdressen[0]->addChild('afa-naam', $this->customer->naam);
+            $aAdressen[0]->addChild('afa-straat', $this->customer->straat);
+            $aAdressen[0]->addChild('afa-huisnummer', $this->customer->huisnummer);
+            $aAdressen[0]->addChild('afa-postcode', $this->customer->postcode);
+            $aAdressen[0]->addChild('afa-plaats', $this->customer->plaats);
+            $aAdressen[0]->addChild('afa-land-code', $this->customer->landCode);
+            $aAdressen[0]->addChild('afa-contactpersoon', $this->customer->contactpersoon);
+            $aAdressen[0]->addChild('afa-telefoon', $this->customer->telefoon);
+            $aAdressen[0]->addChild('afa-e-mailadres', $this->customer->eMailadres);
+        }
 
         $details = $order->addChild('details');
         if(count($this->orderArticles)) {
