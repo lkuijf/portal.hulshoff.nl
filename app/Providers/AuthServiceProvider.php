6d03c5cf29f7c9f3e5d3d6366b0e4a6a672fad2c
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 12:27:05 +0200

    kommaatje niet goed

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,25 +30,25 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            // return (new MailMessage)
-            //     ->greeting(__('Hello') . '!')
-            //     ->subject(__('Verify Email Address'))
-            //     ->line(__('Click the button below to verify your email address') . '.')
-            //     ->action(__('Verify Email Address'), $url)
-            //     ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
-            //     // ->outroLines(['Problemen? Klikken: ' . $url])
-            //     // ->markdown('outroLines', ['aa', 'bb'])
-            //     // ->outroLines = ['a', 'b']
-            //     ;
+            return (new MailMessage)
+                ->greeting(__('Hello') . '!')
+                ->subject(__('Verify Email Address'))
+                ->line(__('Click the button below to verify your email address') . '.')
+                ->action(__('Verify Email Address'), $url)
+                ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
+                // ->outroLines(['Problemen? Klikken: ' . $url])
+                // ->markdown('outroLines', ['aa', 'bb'])
+                // ->outroLines = ['a', 'b']
+                ;
 
-            $msg = new MailMessage();
-            $msg->greeting(__('Hello') . '!');
-            $msg->subject(__('Verify Email Address'));
-            $msg->line(__('Click the button below to verify your email address') . '.');
-            $msg->action(__('Verify Email Address'), $url);
-            $msg->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'));
-            $msg->outroLines = ['cc', 'vv'];
-            return $msg;
+            // $msg = new MailMessage();
+            // $msg->greeting(__('Hello') . '!');
+            // $msg->subject(__('Verify Email Address'));
+            // $msg->line(__('Click the button below to verify your email address') . '.');
+            // $msg->action(__('Verify Email Address'), $url);
+            // $msg->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'));
+            // $msg->outroLines = ['cc', 'vv'];
+            // return $msg;
     });
     }
 }
