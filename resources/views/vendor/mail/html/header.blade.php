a20ac3a71f573b31775dbc6248b116f0ef3b83f3
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 11:19:38 +0200

    removed header link

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,13 +1,13 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+{{-- <a href="{{ $url }}" style="display: inline-block;"> --}}
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {{-- <p>{{ $slot }}</p> --}}
 <img src="{{ url('statics/hulshoff-logo.png') }}" alt="logo" />
 @endif
-</a>
+{{-- </a> --}}
 </td>
 </tr>
