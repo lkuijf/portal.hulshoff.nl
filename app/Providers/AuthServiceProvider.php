727f86ef4616ed276c56dbaeb97c807e49db3a41
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 12:01:00 +0200

    markdown outrolines

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -36,7 +36,8 @@ class AuthServiceProvider extends ServiceProvider
                 ->line(__('Click the button below to verify your email address') . '.')
                 ->action(__('Verify Email Address'), $url)
                 ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
-                ->outroLines(['Problemen? Klikken: ' . $url])
+                // ->outroLines(['Problemen? Klikken: ' . $url])
+                ->markdown('outroLines', ['aa', 'bb'])
                 ;
         });
     }
