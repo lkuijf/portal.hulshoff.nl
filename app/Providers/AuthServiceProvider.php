5fcc6bb3515caa2108c27689a716c38aff4e7460
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 11:23:33 +0200

    translations verfify email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,9 +30,9 @@ class AuthServiceProvider extends ServiceProvider
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
-                ->subject('Verify Email Address2')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address2', $url);
+                ->subject(__('Verify Email Address'))
+                ->line(__('Click the button below to verify your email address') . '.')
+                ->action(__('Verify Email Address'), $url);
         });
     }
 }
