8a6958bbdfa5625a256f99866b703bf94852c053
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-13 11:45:42 +0200

    oke, so using Illuminate\Support\HtmlString now

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\HtmlString;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -34,7 +35,7 @@ class AuthServiceProvider extends ServiceProvider
                 ->subject(__('Verify Email Address'))
                 ->line(__('Click the button below to verify your email address') . '.')
                 ->action(__('Verify Email Address'), $url)
-                ->salutation(__('Regards') . ', \r\n Hulshoff')
+                ->salutation(new HtmlString(__('Regards') . '<br>,Hulshoff'))
                 ;
         });
     }
