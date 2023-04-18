33038e0226fe515d984a8725c9824f5930570cd1
Leon Kuijf <leon.kuijf@gmail.com>
2023-04-18 08:43:17 +0200

    some translations

--- resources/views/mail/order_promoted.blade.php
+++ resources/views/mail/order_promoted.blade.php
@@ -1,12 +1,12 @@
 @extends('templates.email')
 @section('content')
-    <h1>Reservation promoted</h1>
-    <p>Your reservation has been promoted to an order. The products you selected will be delivered on the date you specified. Below you will find details of your order.</p>
-    <p>Order number:<br /><strong>{{ $order->id }}</strong></p>
-    <p>Order code klant:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
-    <p>Delivery date:<br /><strong>{{ date('d-m-Y', strtotime($order->afleverDatum)) }}</strong></p>
+    <h1>{{ __('Reservation promoted') }}</h1>
+    <p>{{ __('Your reservation has been promoted to an order') }}. {{ __('The products you selected will be delivered on the date you specified') }}. {{ __('Below you will find details of your order') }}.</p>
+    <p>{{ __('Order number') }}:<br /><strong>{{ $order->id }}</strong></p>
+    <p>{{ __('Order code customer') }}:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
+    <p>{{ __('Delivery date') }}:<br /><strong>{{ date('d-m-Y', strtotime($order->afleverDatum)) }}</strong></p>
     @if (count($orderProducts))
-        <p>Products:<br />
+        <p>{{ __('Products') }}:<br />
         @foreach ($orderProducts as $prod)
             <strong>{{ $prod }}</strong><br />
         @endforeach
