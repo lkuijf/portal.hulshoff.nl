c405664028587b0c81ff1c7f581d78da98328076
Leon Kuijf <leon.kuijf@gmail.com>
2023-05-01 12:15:33 +0200

    order/reservation link in email

--- resources/views/mail/order_placed.blade.php
+++ resources/views/mail/order_placed.blade.php
@@ -3,12 +3,12 @@
     @if ($order->is_reservation)
         <h1>{{ __('Reservation placed') }}</h1>
         <p>{{ __('Thanks for your reservation') }}. {{ __('The products you selected will be set aside') }}. {{ __('Below you will find details of your reservation') }}.</p>
-        <p>{{ __('Reservation number') }}:<br /><strong>{{ $order->id }}</strong></p>
+        <p>{{ __('Reservation number') }}:<br /><strong><a href="{{ route('reservation_detail', ['id' => $order->id]) }}">{{ $order->id }}</a></strong></p>
         <p>{{ __('Reservation code customer') }}:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
     @else
         <h1>{{ __('Order placed') }}</h1>
         <p>{{ __('Thanks for your order') }}. {{ __('The products you selected will be delivered on the date you specified') }}. {{ __('Below you will find details of your order') }}.</p>
-        <p>{{ __('Order number') }}:<br /><strong>{{ $order->id }}</strong></p>
+        <p>{{ __('Order number') }}:<br /><strong><a href="{{ route('order_detail', ['id' => $order->id]) }}">{{ $order->id }}</a></strong></p>
         <p>{{ __('Order code customer') }}:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
     @endif
     <p>{{ __('Delivery date') }}:<br /><strong>{{ date('d-m-Y', strtotime($order->afleverDatum)) }}</strong></p>
--- resources/views/mail/reservation_reminder.blade.php
+++ resources/views/mail/reservation_reminder.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1>{{ __('Reminder') }}</h1>
     <p>-</p>
-    <p>{{ __('Order number') }}:<br /><strong>{{ $order->id }}</strong></p>
+    <p>{{ __('Order number') }}:<br /><strong><a href="{{ route('reservation_detail', ['id' => $order->id]) }}">{{ $order->id }}</a></strong></p>
     <p>{{ __('Order code customer') }}:<br /><strong>{{ $order->orderCodeKlant }}</strong></p>
     <p>{{ __('Delivery date') }}:<br /><strong>{{ date('d-m-Y', strtotime($order->afleverDatum)) }}</strong></p>
     @if (count($orderProducts))
