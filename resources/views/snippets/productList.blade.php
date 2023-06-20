3889b7231b69db77c60fdff50be57189332ecd06
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-20 16:22:24 +0200

    using file:

--- resources/views/snippets/productList.blade.php
+++ resources/views/snippets/productList.blade.php
@@ -4,7 +4,7 @@
             @foreach($data['products'] as $product)
                 {{-- @include('snippets.product', ['product_id' => $product['id'], 'product_image' => 'https://picsum.photos/300/200', 'product_info' => '<p>' . $product['omschrijving'] . 'Categorie: WERKPLEK</p><p>Inrichtingsconcept: TAFEL</p><p>Leverancier: Gispen</p><p>Soort: VERGADERTAFEL</p>']) --}}
                 {{-- @include('snippets.product', ['product_id' => $product['id'], 'product_code' => $product['artikelCode'], 'product_voorraad' => ($product['voorraad'] - $product['aantal_besteld_onverwerkt']), 'product_image' => 'https://picsum.photos/300/200', 'product_info' => '<p>' . $product['omschrijving'] . '</p>']) --}}
-                @include('snippets.product', ['product_id' => $product['id'], 'product_code' => $product['artikelCode'], 'product_voorraad' => ($product['voorraad'] - $product['aantal_besteld_onverwerkt']), 'product_image' => 'M:\50240\00003.jpg', 'product_info' => '<p>' . $product['omschrijving'] . '</p>'])
+                @include('snippets.product', ['product_id' => $product['id'], 'product_code' => $product['artikelCode'], 'product_voorraad' => ($product['voorraad'] - $product['aantal_besteld_onverwerkt']), 'product_image' => 'file://M:/50240/00003.jpg', 'product_info' => '<p>' . $product['omschrijving'] . '</p>'])
             @endforeach
         @else
             @for ($x=0;$x<12;$x++)
