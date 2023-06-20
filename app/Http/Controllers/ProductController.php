73285fff79c638a858a4cd597276d658bc4e7919
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-20 17:09:22 +0200

    comment

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,7 +95,7 @@ class ProductController extends Controller
         
         $res = $resQry->paginate(10);
 
-dd(Storage::disk('product_images_drive'));
+// dd(Storage::disk('product_images_drive'));
 
         $data = [
             'products' => $res,
