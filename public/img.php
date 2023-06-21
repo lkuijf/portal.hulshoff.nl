a6aa9e692f25b39c40ef14a70a5d7d8f3d38c383
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-21 16:45:36 +0200

    product_images2

--- public/img.php
+++ public/img.php
@@ -7,6 +7,6 @@
 </head>
 <body>
 <!-- <img src="file://192.168.110.37/applications$/MeubelOpslag/Meubelfoto/50944/00009.JPG" alt=""> -->
-    <img src="product_images/50944/00009.JPG" alt="">
+    <img src="product_images2/50944/00009.JPG" alt="">
 </body>
 </html>
