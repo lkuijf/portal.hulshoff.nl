c05f15bf4466f7180180cfefd2dcf2fda68a550a
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-21 16:27:51 +0200

    IMG TEST

---
+++ public/img.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMG TEST</title>
+</head>
+<body>
+    <img src="file://192.168.110.37/applications$/MeubelOpslag/Meubelfoto/50944/00009.JPG" alt="">
+</body>
+</html>
