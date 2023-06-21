86621f963bf87a7cf44e93c4e9cf799d8a6e7f6f
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-21 16:58:53 +0200

    test all

--- public/img.php
+++ public/img.php
@@ -7,6 +7,9 @@
 </head>
 <body>
 <!-- <img src="file://192.168.110.37/applications$/MeubelOpslag/Meubelfoto/50944/00009.JPG" alt=""> -->
+    <img src="product_images/50944/00009.JPG" alt="">
+    <img src="product_images2/50944/00009.JPG" alt="">
+    <img src="product_images3/50944/00009.JPG" alt="">
     <img src="product_images4/50944/00009.JPG" alt="">
 </body>
 </html>
