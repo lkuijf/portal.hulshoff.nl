1c1902e068a767531e8072f9ff0b65e77017df8e
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-26 11:53:09 +0200

    simple accessing file

--- public/simpleaccess.php
+++ public/simpleaccess.php
@@ -1,8 +1,27 @@
 <?php
+$username = 'opslag-user';
+$password = 'casa123!';
+// $remoteFilePath = '\\\\network_drive\\folder\\file.txt';
+$remoteFilePath = '\\\\192.168.110.37\\applications$\\MeubelOpslag\\Meubelfoto\\50246\\00010.jpg';
 
-// $isFolder = is_dir("\\\\192.168.110.37\applications$\meubelopslag\meubelfoto");
-$isFolder = is_dir("\\\\192.168.110.37\applications$");
-var_dump($isFolder); //TRUE
+$handle = fopen($remoteFilePath, 'r', false, stream_context_create([
+    'smb2' => [
+        'username' => $username,
+        'password' => $password
+    ]
+]));
 
-$isFolder = is_dir("//192.168.110.37/applications$");
-var_dump($isFolder); //TRUE
+if ($handle === false) {
+    // Unable to open the file, handle the error
+    die('Unable to access the file.');
+}
+
+// Read the contents of the file
+$fileContents = fread($handle, filesize($remoteFilePath));
+
+// Close the file handle
+fclose($handle);
+
+// Process the file contents as needed
+echo $fileContents;
+?>
