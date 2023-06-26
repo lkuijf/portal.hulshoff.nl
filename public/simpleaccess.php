568a7e8df068ef6a26dc629875cad89dd9b986c0
Leon Kuijf <leon.kuijf@gmail.com>
2023-06-26 11:41:43 +0200

    applications$

--- public/simpleaccess.php
+++ public/simpleaccess.php
@@ -1,7 +1,8 @@
 <?php
 
-$isFolder = is_dir("\\\\192.168.110.37\applications$\meubelopslag\meubelfoto");
+// $isFolder = is_dir("\\\\192.168.110.37\applications$\meubelopslag\meubelfoto");
+$isFolder = is_dir("\\\\192.168.110.37\applications$");
 var_dump($isFolder); //TRUE
 
-// $isFolder = is_dir("//NAS/Main Disk");
-// var_dump($isFolder); //TRUE
+$isFolder = is_dir("//192.168.110.37/applications$");
+var_dump($isFolder); //TRUE
