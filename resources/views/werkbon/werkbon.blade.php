b1b1ca64e6b85d658267b2c7d2c6cc3b3b09d2df
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-09 12:19:55 +0200

    better styling

--- resources/views/werkbon/werkbon.blade.php
+++ resources/views/werkbon/werkbon.blade.php
@@ -1,16 +1,25 @@
-{{ $hulshoffUser->name }}-
-{{ $hulshoffUser->email }}-
-{{ $customer->klantCode }}-
-{{ $customer->naam }}-
-{{ $address->naam }}-
-{{ $address->straat }}-
-{{ $address->huisnummer }}-
-{{ $address->postcode }}-
-{{ $address->plaats }}-
-{{ $address->landCode }}-
-{{ $address->contactpersoon }}-
-{{ $address->telefoon }}-
-{{ $address->eMailadres }}-
+<h2>Melder gegevens</h2>
+<p>
+    Naam melder: <strong>{{ $hulshoffUser->name }}</strong><br>
+    E-mail melder: <strong>{{ $hulshoffUser->email }}</strong>
+</p>
+<h2>Klant gegevens</h2>
+<p>
+    KlantCode: <strong>{{ $customer->klantCode }}</strong><br>
+    Klant naam: <strong>{{ $customer->naam }}</strong><br>
+</p>
+<h2>Afleveradres</h2>
+<p>
+    Naam: <strong>{{ $address->naam }}</strong><br>
+    Straat: <strong>{{ $address->straat }}</strong><br>
+    Huisnummer: <strong>{{ $address->huisnummer }}</strong><br>
+    Postcode: <strong>{{ $address->postcode }}</strong><br>
+    Plaats: <strong>{{ $address->plaats }}</strong><br>
+    Landcode: <strong>{{ $address->landCode }}</strong><br>
+    Contactpersoon: <strong>{{ $address->contactpersoon }}</strong><br>
+    Telefoonnummer: <strong>{{ $address->telefoon }}</strong><br>
+    E-mail adres: <strong>{{ $address->eMailadres }}</strong><br>
+</p>
 @if ($products && count($products))
     <table>
         <tr>
