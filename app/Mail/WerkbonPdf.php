53addc1cac15a3607e19b0a406adabe3c042f8f3
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-09 08:53:52 +0200

    using Illuminate\Mail\Mailables\Attachment

--- app/Mail/WerkbonPdf.php
+++ app/Mail/WerkbonPdf.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Queue\SerializesModels;
 
 class WerkbonPdf extends Mailable
