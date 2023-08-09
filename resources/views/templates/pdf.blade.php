fc868768d827ab75b6baa944467946bfbeb0c115
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-09 09:29:42 +0200

    back to origin

--- resources/views/templates/pdf.blade.php
+++ resources/views/templates/pdf.blade.php
@@ -33,9 +33,9 @@
 </head>
 <body>
     <header>
-        {{-- <img src="{{ asset('statics/hulshoff-logo.png') }}" alt="Hulshoff"> --}}
+        <img src="{{ asset('statics/hulshoff-logo.png') }}" alt="Hulshoff">
         {{-- <img src="https://opslag2.hulshoff.nl/statics/hulshoff-logo.png" alt="Hulshoff"> --}}
-        <img src="data:image/png;base64,{{ base64_encode(file_get_contents('https://opslag2.hulshoff.nl/statics/hulshoff-logo.png')) }}" alt="Hulshoff">
+        {{-- <img src="data:image/png;base64,{{ base64_encode(file_get_contents('https://opslag2.hulshoff.nl/statics/hulshoff-logo.png')) }}" alt="Hulshoff"> --}}
         <div>
             <p>
                 Periode: {{ $period }}<br />
