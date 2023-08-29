091c76e6dc06b959c7c93db6e482cc07a8f9efe4
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-29 12:08:47 +0200

    better email styling

--- resources/views/mail/minimum_stock_notify.blade.php
+++ resources/views/mail/minimum_stock_notify.blade.php
@@ -3,30 +3,29 @@
     <h1>{{ __('Below mimium stock') }}</h1>
     <p>{{ __('The stock of some products has fallen below the minimum stock') }}.</p>
     @if (count($products))
-        <p>{{ __('Products') }}:</p>
-        <table>
-            <tr>
-                <th>klantCode</th>
-                <th>artikelCode</th>
-                <th>artikelCodeKlant</th>
-                <th>omschrijving</th>
-                <th>Minimale voorraad</th>
-                <th>Actuele voorraad</th>
-                <th>Aantal gereserveerd</th>
-                <th>Aantal besteld</th>
-            </tr>
+        <h2>{{ __('Products') }}:</h2>
         @foreach ($products as $prod)
-            <tr>
-                <td>{{ $prod->klantCode }}</td>
-                <td>{{ $prod->artikelCode }}</td>
-                <td>{{ $prod->artikelCodeKlant }}</td>
-                <td>{{ $prod->omschrijving }}</td>
-                <td>{{ $prod->minimaleVoorraad }}</td>
-                <td>{{ $prod->voorraad }}</td>
-                <td>{{ $prod->reservedAmount() }}</td>
-                <td>{{ $prod->orderedAmount() }}</td>
-            </tr>
+            <p>
+                klantCode: <strong>{{ $prod->klantCode }}</strong><br>
+                artikelCode: <strong>{{ $prod->artikelCode }}</strong><br>
+                artikelCodeKlant: <strong>{{ $prod->artikelCodeKlant }}</strong><br>
+                Omschrijving: <strong>{{ $prod->omschrijving }}</strong><br>
+            </p>
+            <table>
+                <tr>
+                    <th>Minimale voorraad</th>
+                    <th>Actuele voorraad</th>
+                    <th>Aantal gereserveerd</th>
+                    <th>Aantal besteld</th>
+                </tr>
+                <tr>
+                    <td>{{ $prod->minimaleVoorraad }}</td>
+                    <td>{{ $prod->voorraad }}</td>
+                    <td>{{ $prod->reservedAmount() }}</td>
+                    <td>{{ $prod->orderedAmount() }}</td>
+                </tr>
+            </table>
+            <hr>
         @endforeach
-        </table>
     @endif
 @endsection
