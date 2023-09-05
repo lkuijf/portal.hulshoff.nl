a58d7fe64bf3939474d35fc11f3d0461ba67a545
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-05 10:43:14 +0200

    enabled reset passowrd texts

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -39,16 +39,16 @@ class AuthServiceProvider extends ServiceProvider
                 ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
                 ;
         });
-        // ResetPassword::toMailUsing(function ($notifiable, $url) {
-        //     return (new MailMessage)
-        //         ->greeting(__('Hello') . '!')
-        //         ->subject(__('Reset Password'))
-        //         ->line(__('You are receiving this email because we received a password reset request for your account') . '.')
-        //         ->action(__('Reset Password'), $url)
-        //         ->line(__('This password reset link will expire in 60 minutes') . '.')
-        //         ->line(__('If you did not request a password reset, no further action is required') . '.')
-        //         ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
-        //         ;
-        // });
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->greeting(__('Hello') . '!')
+                ->subject(__('Reset Password'))
+                ->line(__('You are receiving this email because we received a password reset request for your account') . '.')
+                ->action(__('Reset Password'), $url)
+                ->line(__('This password reset link will expire in 60 minutes') . '.')
+                ->line(__('If you did not request a password reset, no further action is required') . '.')
+                ->salutation(new HtmlString(__('Regards') . ',<br>Hulshoff'))
+                ;
+        });
     }
 }
