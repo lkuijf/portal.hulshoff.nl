b98cc4d6201c6aa380845991129dbaf9d8e5866b
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-05 11:23:20 +0200

    translation files added

---
+++ lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Uw wachtwoord is gereset!',
+    'sent' => 'U heeft een e-mail ontvangen met een wachtwoord reset link!',
+    'throttled' => 'Wacht even voordat u het opnieuw probeert.',
+    'token' => 'Dit wachtwoord token is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met dat e-mail adres.",
+
+];
