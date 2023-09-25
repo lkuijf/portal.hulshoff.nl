189cbba4ea8ba4393af4b99bee3783451392bbb1
Leon Kuijf <leon.kuijf@gmail.com>
2023-09-25 08:32:11 +0200

    edit copy_of_order_confirmation for testing

--- config/hulshoff.php
+++ config/hulshoff.php
@@ -13,5 +13,5 @@ return [
     'privileges' => ['show_tiles', 'filter_on_top', 'filter_at_side'],
     'archiveXmlWhenOlderThanXDays' => 7,
     'OrdersOutEndpoint' => 'https://edi.hulshoff.nl/api/accept/interface-wti/BerichtOrders',
-    'copy_of_order_confirmation' => ['leon.kuijf@gmail.com', 'leon@wtmedia-events.nl'],
+    'copy_of_order_confirmation' => ['planning2@hulshoff.nl'],
 ];
