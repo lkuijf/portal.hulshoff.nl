dc45b54b96d623f8181a97916506604fdc7286b9
Leon Kuijf <leon.kuijf@gmail.com>
2023-11-29 15:00:19 +0100

    extra 2fa message when no data available

--- resources/views/no-data.blade.php
+++ resources/views/no-data.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <div class="noDataContent">
         <h1>{{ __('No data found') }}</h1>
-        <p>{{ __('Check if you have selected a customer') }}.</p>
+        <p>- {{ __('Check if you have selected a customer') }}.</p>
+        <p>- {{ __('Check if you have enabled Two Factor Authentication') }}.</p>
     </div>
 @endsection
