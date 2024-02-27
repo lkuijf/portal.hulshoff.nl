6b452786369b4ec56fdf830d27a235a31bf3c2e5
Leon Kuijf <leon.kuijf@gmail.com>
2024-02-27 15:43:12 +0100

    when user is removed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -82,14 +82,14 @@ class Product extends Model
         if($orderArticles) {
             foreach($orderArticles as $orderArt) {
                 if($orderArt->order->is_reservation) {
-                    if($orderArt->order->hulshoffUser) {
+                    //if($orderArt->order->hulshoffUser) {
                         $resInfo = new \stdClass();
                         $resInfo->orderId = $orderArt->order->id;
-                        $resInfo->orderUserName = $orderArt->order->hulshoffUser->name;
-                        $resInfo->orderUserEmail = $orderArt->order->hulshoffUser->email;
+                        $resInfo->orderUserName = ($orderArt->order->hulshoffUser->name?$orderArt->order->hulshoffUser->name:'- gebruiker bestaat niet meer -');
+                        $resInfo->orderUserEmail = ($orderArt->order->hulshoffUser->email?$orderArt->order->hulshoffUser->email:'- gebruiker bestaat niet meer -');
                         $resInfo->amount = $orderArt->amount;
                         $reservations[] = $resInfo;
-                    }
+                   // }
                 }
             }
         }
